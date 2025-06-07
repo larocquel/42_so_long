9e1d3a308a2ec4a405e3457c8a779accdc4db84d
larocquel <leonardolarocque@gmail.com>
2025-06-07 17:34:19 +0100

    fixed main.c

--- src/main.c
+++ src/main.c
@@ -6,16 +6,21 @@
 /*   By: leoaguia <leoaguia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 16:33:09 by leoaguia          #+#    #+#             */
-/*   Updated: 2025/06/07 17:09:56 by leoaguia         ###   ########.fr       */
+/*   Updated: 2025/06/07 17:33:34 by leoaguia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-int	main(int ac, char **av)
+int	main(void)
 {
-	(void) ac;
-	(void) av;
+	t_game	game;
 
-	ft_printf("Hello, World!\n");
+	game.mlx = mlx_init();
+	if (!game.mlx)
+		return (1);
+	game.win = mlx_new_window(game.mlx, WIDTH, HEIGHT, "Hello, World!");
+	if (!game.win)
+		return (free(game.mlx), 1);
+	mlx_loop(game.mlx);
 }
